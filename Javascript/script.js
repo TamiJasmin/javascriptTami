1f964554f04d05005e1377127c35e0adb45e0e72
Tamara Jasmin <tamara.veronamado@gmail.com>
2022-09-21 12:24:21 -0300

    new files

---
+++ Javascript/script.js
@@ -0,0 +1,16 @@
+
+/* Funciones */
+
+function saludar () {
+    let userName = prompt('Hola, ¿podés ingresar tu nombre?');
+    alert ('Hola ' + userName);
+
+}
+
+saludar ();
+
+/* Ciclos */
+
+
+
+
